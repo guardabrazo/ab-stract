c24ca9fb4f5f53c23a9ef12a236a01e592effa4f
guardabrazo <guardabrazo@iMac-de-Federico-2.local>
2015-03-05 11:27:05 +0100

    Initial Commit

---
+++ ab·stract/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ab·stract
+//
+//  Created by guardabrazo on 5/3/15.
+//  Copyright (c) 2015 Espadaysantacruz Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ab·stract/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ab·stract
+//
+//  Created by guardabrazo on 5/3/15.
+//  Copyright (c) 2015 Espadaysantacruz Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
